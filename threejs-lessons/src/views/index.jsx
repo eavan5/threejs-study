a441942f87aba0ba8cce8a46df29b187b00aa523
Eavan <webwwy@gmail.com>
2023-04-26 21:30:25 +0800

    光源和材质

--- threejs-lessons/src/views/index.jsx
+++ threejs-lessons/src/views/index.jsx
@@ -12,8 +12,13 @@ const Page = () => {
 		const scene = new THREE.Scene()
 		// 创建立方体
 		const geometry = new THREE.BoxGeometry(1, 1, 1)
-		// 创建材质
-		const material = new THREE.MeshBasicMaterial({
+		// 创建材质 //基础材质，不受光照影响
+		// const material = new THREE.MeshBasicMaterial({
+		// 	color: 0xff0000,
+		// })
+
+		// 创建材质 //网格材质，受光照影响
+		const material = new THREE.MeshLambertMaterial({
 			color: 0xff0000,
 		})
 
@@ -21,6 +26,17 @@ const Page = () => {
 		const mesh = new THREE.Mesh(geometry, material)
 		// 添加到场景
 		scene.add(mesh)
+
+		// 创建坐标轴
+		const axesHelper = new THREE.AxesHelper(5)
+		scene.add(axesHelper)
+
+    // 创建全局光源
+    const ambientLight = new THREE.AmbientLight(0xffffff,0.5)
+    // 创建方向光源
+    const directionalLight = new THREE.DirectionalLight(0xffffff,0.5)
+    
+    scene.add(ambientLight, directionalLight)
 
 		// 创建相机
 		const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
@@ -35,7 +51,13 @@ const Page = () => {
 		// 创建渲染器
 		const renderer = new THREE.WebGLRenderer({
 			canvas,
+			// 抗锯齿
+			antialias: true,
 		})
+
+		// 设置像素比
+		renderer.setPixelRatio(window.devicePixelRatio)
+
 		// 设置渲染器大小
 		renderer.setSize(width, height)
 
